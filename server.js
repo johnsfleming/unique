ccc07533dfd6cd581bd8dd20729f7a131db2d149
FlemSquad <john.s.fleming@gmail.com>
2017-08-09 20:16:12 -0400

    Trying to bind selection to display

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect("mongodb://heroku_4hl01spr:lhnj84jka2921l2o9hqi5c1t3h@ds161041.mlab.com:61041/heroku_4hl01spr", function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
